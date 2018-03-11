21313d3ae421cf39ae7a560899225f691954b280
AkshayRajaramanSubramanian <36255890+AkshayRajaramanSubramanian@users.noreply.github.com>
2018-03-11 15:54:26 -0400

    v2
    Gets functions performed now in string

--- render.py
+++ render.py
@@ -1,7 +1,30 @@
-from flask import *
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 11 14:12:16 2018
 
+@author: Akshay Rajaraman
+"""
+from flask import Flask,render_template,request
+import json
 app = Flask(__name__)
+@app.route("/")
+#def index(name=None):
+#    return render_template('JHacks.html', name=name)
 
-@app.route('/')
 def index(name=None):
     return render_template('JHacks.html', name=name)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['JSON']
+    processed_text = text
+    parsed_json = json.loads(processed_text)
+    nodeDataArray_json = parsed_json['nodeDataArray']
+    category_string = ""
+    for i in nodeDataArray_json:
+        category_string = category_string+ i['category']+" "
+    
+    return category_string
+    
+if __name__=="__main__":
+    app.run()
