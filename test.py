6a95bdb70cab3dc4ad332563b7b2a2ec8b362ba5
AkshayRajaramanSubramanian <36255890+AkshayRajaramanSubramanian@users.noreply.github.com>
2018-03-11 03:57:01 -0400

    Code v2
    Added matlab and python snippets as well

--- test.py
+++ test.py
@@ -27,10 +27,59 @@ class cpp:
 
 		return code
 
+class python:
+	def __init__(self):
+		pass
+
+	def forloop(self, start, operator, stop, child):
+		code = """for i in range(%s,%s):%s\n""" %(start, stop, """\t"""+str(self.child_opts(child)))
+		
+		return code
+
+	def ifstat(self, cond, child):
+		code =  """if %s: \t%s""" %(cond,"""\t"""+str(self.child_opts(child)))
+		return code
+
+	def init(self, var, val):
+		code = """%s = %s;""" %(var, val)
+		return code
+
+	def child_opts(self, child):
+		num_opts = len(child)
+		code = ""
+		for item in child:
+			code += """\n\t%s""" %(item)
+
+		return code
+
+class matlab:
+	def __init__(self):
+		pass
+
+	def forloop(self, start, operator, stop, child):
+		code = """for i = %s:%s\n\t %s\nend\n""" %(start, stop, str(self.child_opts(child)))
+		
+		return code
+
+	def ifstat(self, cond, child):
+		code =  """if (%s) \n\t%s\n\tend\b""" %(cond,str(self.child_opts(child)))
+		return code
+
+	def init(self, var, val):
+		code = """%s = %s;""" %(var, val)
+		return code
+
+	def child_opts(self, child):
+		num_opts = len(child)
+		code = ""
+		for item in child:
+			code += """\n\t%s""" %(item)
+
+		return code
 
 
 
-x = cpp()
+x = python()
 inits = [];
 for i in range(5):
 	init1 = x.init("a", str(i))
@@ -41,5 +90,4 @@ ifs = x.ifstat("i <= 10", inits)
 
 loop = x.forloop("1", "<=", "10", [ifs])
 
-print ifs
-print loop
+print (loop)
